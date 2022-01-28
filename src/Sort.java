dfbd5d5e5ef5a9b7709f56041b106a54a087ba05
Armaan H <armaanhajar@icloud.com>
2022-01-27 17:49:23 -0800

    Commented
    Added comments to code

--- src/Sort.java
+++ src/Sort.java
@@ -1,3 +1,9 @@
+/*
+ * This program that will sort a list of number and print it to the console as well as an output file.
+ * Author: Armaan Hajarizadeh
+ * Date: 1/5/2022
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Iterator;
@@ -7,7 +13,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Sort {
-
 	Scanner consoleInput = new Scanner(System.in);
 	String input;
 	Scanner fileInput;
@@ -15,6 +20,7 @@ public class Sort {
 	long startTime;
 	
 	public Sort() {
+		// choose one of the 4 input files with a list of numbers (input5.txt is a test file I was using)
 		System.out.println("Enter a number for the input file.");
 		System.out.println("1: input1.txt   2: input2.txt   3: input3.txt   4: input4.txt   5: input5.txt");
 		input = consoleInput.nextLine();
@@ -26,12 +32,17 @@ public class Sort {
 				input = consoleInput.nextLine();
 			}
 		}
-		try {
+		
+		startTime = System.currentTimeMillis(); // starts timer
+
+		try { // finds the file in your computer
 			fileInput = new Scanner(new File("input" + input.charAt(0) + ".txt"));
 		} catch (FileNotFoundException ex) {
 			ex.printStackTrace();
 			System.exit(0);
 		}
+		
+		// inputs the file and parses into an integer array
 		String infile = fileInput.nextLine();
 		String[] inputStringArray = infile.split(",");
 		inputArray = new int[inputStringArray.length];
@@ -39,6 +50,8 @@ public class Sort {
 			inputArray[i] = Integer.parseInt(inputStringArray[i]);
 			System.out.println(inputArray[i]);
 		}
+		
+		// choose one of the 4 sorting methods to sort the numbers in the file you choose
 		System.out.println("Enter a number for the sort you want to use.");
 		System.out.println("1: Bubble   2: Selection   3: Table   4: Quick Sort");
 		input = consoleInput.nextLine();
@@ -50,29 +63,32 @@ public class Sort {
 				input = consoleInput.nextLine();
 			}
 		}
-		
-		startTime = System.currentTimeMillis();
-		if (input.equals("1")) {
+				
+		// checks to see what sorting method you chose and calls the method
+		if (input.equals("1")) { // calls bubble sort
 			inputArray = bubbleSort(inputArray);
 		}
-		if (input.equals("2")) {
+		if (input.equals("2")) { // calls selection sort
 			inputArray = selectionSort(inputArray);
 		}
-		if (input.equals("3")) {
+		if (input.equals("3")) { // calls table sort
 			inputArray = tableSort(inputArray);
 		}
-		if (input.equals("4")) {
+		if (input.equals("4")) { // calls quick sort
 			quickSort(inputArray, 0, inputArray.length - 1);
 		}
-		long totalTime = System.currentTimeMillis() - startTime;
+		long totalTime = System.currentTimeMillis() - startTime; // ends timer
 		PrintWriter pw;
 		
+		// prints output to an output file
 		try {
 			pw = new PrintWriter(new FileWriter(new File("output.txt")));
 			String output = "";
 			for (int i = 0; i < inputArray.length; i++) {
 				output += inputArray[i] +",";
 			}
+			
+			// adds the total time taken to complete the program to the output file
 			output += "\nTotal Time: " + totalTime + " Milliseconds";
 			pw.write(output);
 			pw.close();
@@ -81,6 +97,7 @@ public class Sort {
 			System.exit(0);
 		}
 
+		// prints an empty line and the total time taken to the console
 		for (int i = 0; i < inputArray.length; i++) {
 			System.out.println(inputArray[i]);
 		}
@@ -88,7 +105,10 @@ public class Sort {
 		System.out.println("Total Time: " + totalTime + " Milliseconds");
 	}
 	
-	// compare each pair of numbers and move the larger to the right
+	/* 
+	 * BUBBLE SORT
+	 * Compare each pair of numbers and move the larger to the right
+	 */
 	int[] bubbleSort(int[] array) {
 		for (int j = 0; j < array.length; j++) {
 			for (int i = 0; i < array.length - 1; i++) {
@@ -104,7 +124,10 @@ public class Sort {
 		return array;
 	}
 	
-	// find the smallest and move it to the from
+	/*
+	 * SELECTION SORT
+	 * Find the smallest and move it to the front
+	 */
 	int[] selectionSort(int[] array) {
 		for (int j = 0; j < array.length; j++) {
 			int smallestNumber = array[j];
@@ -115,6 +138,7 @@ public class Sort {
 					smallestIndex = i;
 				}
 			}
+			// swaps numbers
 			int temp = array[smallestIndex];
 			array[smallestIndex] = array[j];
 			array[j] = temp;
@@ -123,7 +147,10 @@ public class Sort {
 		return array;
 	}
 	
-	// tally how often you see each number, print out that number of times
+	/* 
+	 * TABLE SORT (a.k.a. Counting Sort)
+	 * Tally how often you see each number, prints out that number of times
+	 */
 	int [] tableSort(int[] array) {
 		int[] tally = new int[1001];
 		for (int i = 0; i < array.length; i++) {
@@ -141,6 +168,14 @@ public class Sort {
 		return array;
 	}
 	
+	/*
+	 * QUICK SORT
+	 * Choose a pivot point
+	 * Move all numbers bigger than the pivot to the right of the number
+	 * Move all the numbers smaller than the pivot to the left of the number
+	 * Run quick sort again on the numbers to the left and right of the original pivot
+	 * Repeat steps until numbers are sorted
+	 */
 	void quickSort(int[] array, int left, int right) {
 		if (left < right) {
 			int a = qsPartition(array, left, right);
@@ -151,12 +186,14 @@ public class Sort {
 	}
 	
 	int qsPartition(int[] array, int firstNumber, int lastNumber) {
+		// setting the pivot, smallest number, and smallest index
 		int pivot = firstNumber; 
 		int smallestNumber = array[firstNumber];
 		int smallestIndex = firstNumber + 1;
 		
 		for (int j = firstNumber + 1; j <= lastNumber; j++) {
 				if (array[j] < array[pivot]) {
+					// swaps numbers
 					int temp = array[j];
 					array[j] = array[smallestIndex];
 					array[smallestIndex] = temp;
@@ -164,7 +201,7 @@ public class Sort {
 					smallestIndex++;
 				}
 			}
-		
+		// swaps the pivot in between the right and left partition
 		int temp = array[smallestIndex - 1];
 		array[smallestIndex - 1] = array[pivot];
 		array[pivot] = temp;
